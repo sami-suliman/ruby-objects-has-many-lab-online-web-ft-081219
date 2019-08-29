cf1a334b1bc4c255c7467adb671d6b2313ba047f
sami suliman <sami.bco@gmail.com>
2019-08-29 11:22:23 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,18 @@
+class Artist 
+  attr_accessor :name
+  
+  def initialize(name)
+    @songs = []
+    @name = name
+  end
+
+  def songs
+    @songs
+  end
+  
+   def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+end 
